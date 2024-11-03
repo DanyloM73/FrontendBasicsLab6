14183ddd465b07cf074bc1f84aa74731d2ee15b1
Danylo Malanin <115399817+DanyloM73@users.noreply.github.com>
2024-11-03 18:51:28 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function fetchUsers() {
+  fetch('https://randomuser.me/api/?results=5')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      displayUsers(data.results);
+    })
+    .catch(error => {
+      console.error('An error occurred while receiving data:', error);
+    });
+}
+
+function displayUsers(users) {
+  const userContainer = document.getElementById('user-container');
+  userContainer.innerHTML = '';
+
+  users.forEach(user => {
+    const userCard = document.createElement('div');
+    userCard.classList.add('user-card');
+    
+    userCard.innerHTML = `
+      <img src="${user.picture.large}" alt="User Picture">
+      <p class="user-info"><strong>Name:</strong> ${user.name.first} ${user.name.last}</p>
+      <p class="user-info"><strong>City:</strong> ${user.location.city}</p>
+      <p class="user-info"><strong>Post index:</strong> ${user.location.postcode}</p>
+      <p class="user-info"><strong>Phone:</strong> ${user.phone}</p>
+    `;
+      
+    userContainer.appendChild(userCard);
+  });
+
+  userContainer.style.display = 'flex';
+}
+
+document.querySelector('#loadButton').addEventListener('click', () => fetchUsers());
